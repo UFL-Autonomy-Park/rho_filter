0f5af79d294d07f2a641baf59c1531e8d733810b
Max Gardenswartz <max.gardenswartz@proton.me>
2025-11-19 17:17:53 -0500

    Started to prepare for adding a header file

--- src/rho_filter.cpp
+++ src/rho_filter.cpp
@@ -3,17 +3,44 @@
 #include <unsupported/Eigen/MatrixFunctions>
 #include <Eigen/Dense>
 
+
+
 using namespace std;
 
-double main(){
-    double sampling_time = 0.01; // seconds
-    int n = 3; // State space dim
-    int dim = 4;
-    double alpha = 0.0f;
-    double k1 = 0.0f;
-    double k2 = 0.0f;
-    double k3 = 0.0f;
 
+template <typename T>
+int signum(T x){
+    return (x < 0) ? -1 : (x > 0);
+}
+
+template <typename T>
+T matrix_signum_elementwise(const T& A) {
+    return A.unaryExpr([](double x) {
+        return (double)signum(x); 
+    });
+}
+
+
+
+Rho_Filter::Rho_Filter(
+    double sampling_time, // seconds
+    int state_space_dim, // State space dim
+    double alpha,
+    double k1,
+    double k2,
+    double k3)
+   : sampling_time(sampling_time),
+      alpha(alpha),
+      k1(k1),
+      k2(k2),
+      k3(k3),
+
+      M(4,4),
+      N(4,1),
+      E(4,1),
+      S(1,4),
+{
+    int dim = n;
     double m_12 = -(3 - pow(k1, 2) + (2*k1 + k2 + k3) * (k1 + k2));
     double m_13 = -(k1 + k2);
     double m_14 = -(1 + (k1 + k2)*(k3 - k1));
@@ -47,31 +74,17 @@ double main(){
     Eigen::MatrixXd S(1,dim);
     S << -1, 0, -1, 0;
 
-    double propagate_filter(double last_position){
-        // Initial filter state
-        Eigen::MatrixXd zeta(4*n,1) // zeta = [ q_hat, p_hat, m, sigma]
+    // Filter matrices
+    Eigen::MatrixXd M_inv = M.colPivHouseholderQr().inverse();
+    Eigen::MatrixXd A_d = (M * sampling_time).exp();
+    Eigen::MatrixXd I_n = Eigen::MatrixXd::Identity(4, 4);
+    Eigen::MatrixXd B_q = M_inv * (A_d - I_n) * N;
+    Eigen::MatrixXd B_s = M_inv * (A_d - I_n) * E;
 
-        double  = 0; // q_k
-        Eigen::MatrixXd M_inv = M.colPivHouseholderQr().inverse();
-        Eigen::MatrixXd A_d = (M * sampling_time).exp();
-        Eigen::MatrixXd I_n = Eigen::MatrixXd::Identity(4, 4);
-        Eigen::MatrixXd B_q = M_inv * (A_d - I_n) * N;
-        Eigen::MatrixXd B_s = M_inv * (A_d - I_n) * E;
-
-        zeta = A_d * zeta + B_q * last_position + alpha * B_s * matrix_signum_elementwise(S * zeta + last_position)
-    }
-    
-    return 0;
+    // Initial filter state
+    Eigen::MatrixXd zeta(4*n,1) // zeta = [ q_hat, p_hat, m, sigma]
 }
 
-template <typename T>
-int signum(T x){
-    return (x < 0) ? -1 : (x > 0);
-}
-
-template <typename T>
-T matrix_signum_elementwise(const T& A) {
-    return A.unaryExpr([](double x) {
-        return (double)signum(x); 
-    });
+void Rho_Filter::propagate_filter(double last_position){ // q_k
+    zeta = A_d * zeta + B_q * last_position + alpha * B_s * matrix_signum_elementwise(S * zeta + last_position)
 }
